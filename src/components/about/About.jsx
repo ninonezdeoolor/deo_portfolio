de5515a3614f31309aa7e3a5e61867df00690bba
ninonezdeoolor <ninonezdeoolor08@gmail.com>
2023-08-16 19:00:48 +0800

    Edit About Me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
             <article className='about__card'>
               <FiUsers className='about__icon'/>
               <h5 className='about__label'>Clients</h5>
-              <small>2+ Worlwide</small>
+              <small>2+ Worldwide</small>
             </article>
 
             <article className='about__card'>
